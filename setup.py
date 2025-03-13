a51cc3dee0ac6f12459fa79c53fd8ef189145210
Haeuk Kim <hkim@lji.org>
2025-03-13 00:53:53 -0700

    Removing self dependency from setup.py and bumping up the version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
   name = 'ngargparser',         
-  version = '0.1.6',      
+  version = '0.1.7',      
   license='MIT',
   description = 'This is a standardized parser framework for CLI tools. This class will enforce certain properties or abstract methods to be implemented to properly create an Argument Parser class for other CLI tools.',   # Give a short description about your library
   author = 'Haeuk Kim',                 
@@ -20,7 +20,7 @@ setup(
       'ngargparser.templates': ['**/*'],
   },
   include_package_data = True,
-  install_requires = ['ngargparser'],
+  install_requires = [],
   entry_points = {
     'console_scripts': [
           'cli = ngargparser.cli:main',
